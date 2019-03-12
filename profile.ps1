e0364f4deaefa6ec15badbee1c34f732f32135cd
Guo <msn4399@163.com>
2019-03-12 10:51:42 +0800

    powershell profile

---
+++ profile.ps1
@@ -0,0 +1,8 @@
+# 在当前目录打开资源管理器
+function e { explorer . }
+
+# Git 缩写
+function gss { git status -s }
+function gpush { git push }
+function gpl { git pull }
+function gcam($msg) { git commit -a -m $msg }
